d611eed3cb3b5e2bc96c471d5fc7c46ae959af3a
Keanan Enrico <enricokeanan@gmail.com>
2025-09-01 22:34:11 +0700

    COMP6047001 - Algorithm and Programming(Tugas FYP2)

--- TugasFYP2.c
+++ TugasFYP2.c
@@ -1,6 +1,1 @@
-#include <stdio.h>
-int main(){
-    printf("Halo! Nama saya adalah Keanan Enrico\n");
-    printf("Bahasa pemrograman favorite saya adalah Python!\n");
-    return 0;
-}
+//COMP6047001 - Algorithm and Programming
